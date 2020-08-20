1ef0730d460a2d8ce7e51995397696986ab5b91b
Gregory Hill <gregorydhill@outlook.com>
2020-08-20 11:52:52 +0100

    Updates

---
+++ precache-manifest.894eab8d4eaaf98a663b508b93e05db3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdfe16eb34daa0ad70082b9652bd1c67",
+    "url": "/ledger-btc-react/index.html"
+  },
+  {
+    "revision": "ac7a88d85a2bdb3b0207",
+    "url": "/ledger-btc-react/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "fcfb6f72cacbff25a1c0",
+    "url": "/ledger-btc-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ac7a88d85a2bdb3b0207",
+    "url": "/ledger-btc-react/static/js/2.df6681d9.chunk.js"
+  },
+  {
+    "revision": "d2231858b720746bbf4f94cdd4f4c082",
+    "url": "/ledger-btc-react/static/js/2.df6681d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcfb6f72cacbff25a1c0",
+    "url": "/ledger-btc-react/static/js/main.32fd55cc.chunk.js"
+  },
+  {
+    "revision": "3ed6901eab29d7c1ee88",
+    "url": "/ledger-btc-react/static/js/runtime-main.7dba9b34.js"
+  }
+]);
