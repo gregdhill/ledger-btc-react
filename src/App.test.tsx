589b6ec313bcb720cfe05e95ee3f2aaa9cfc174a
Gregory Hill <gregorydhill@outlook.com>
2020-08-21 11:49:33 +0100

    add basic tests and runner
    Signed-off-by: Gregory Hill <gregorydhill@outlook.com>

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// https://create-react-app.dev/docs/running-tests
+// https://devhints.io/enzyme
+
+it("renders without crashing", () => {
+  shallow(<App />);
+});
+
+it("should try to connect", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find("Button").text()).toEqual("Connect");
+  wrapper.setState({ isConnecting: true });
+  expect(wrapper.find("Button").text()).toEqual("Loading...");
 });
